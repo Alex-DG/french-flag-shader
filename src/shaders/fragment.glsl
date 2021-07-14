0e3c69a7ae129a5587c5c8a64d67ab9bf10298fe
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-07-14 14:50:53 +0100

    Change the texture color with the elevation

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -4,10 +4,14 @@ uniform vec3 uColor;
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main()
 {
     vec4 textureColor = texture2D(uTexture, vUv);
     // gl_FragColor = vec4(uColor, 1.0);
+
+    textureColor.rgb *= vElevation * 1.8 + 0.8;
+
     gl_FragColor = textureColor;
 }
