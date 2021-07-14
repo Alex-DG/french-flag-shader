6914e33b5258302c064691ea6c718570c129dc53
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-07-14 14:40:24 +0100

    Add color control

--- src/script.js
+++ src/script.js
@@ -5,12 +5,16 @@ import * as dat from 'dat.gui'
 
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
+import { generateUUID } from 'three/src/math/MathUtils'
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+const debugOptions = {
+  color: '#8a2be2',
+}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,18 +33,47 @@ const textureLoader = new THREE.TextureLoader()
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
+const count = geometry.attributes.position.count
+const randoms = new Float32Array(count)
+
+for (let i = 0; i < count; i++) {
+  randoms[i] = Math.random()
+}
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
+
 // Material
 const material = new THREE.RawShaderMaterial({
   vertexShader,
   fragmentShader,
+  // transparent: true,
   side: THREE.DoubleSide,
   uniforms: {
     uTime: { value: 0 },
+    uFrequency: { value: new THREE.Vector2(10, 2) },
+    uColor: { value: new THREE.Color(debugOptions.color) },
   },
 })
 
+// Debug
+gui
+  .add(material.uniforms.uFrequency.value, 'x')
+  .min(0)
+  .max(20)
+  .step(0.001)
+  .name('frequencyX')
+gui
+  .add(material.uniforms.uFrequency.value, 'y')
+  .min(0)
+  .max(20)
+  .step(0.001)
+  .name('frequencyY')
+gui
+  .addColor(debugOptions, 'color')
+  .onChange(() => material.uniforms.uColor.value.set(debugOptions.color))
+
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
+mesh.scale.y = 2 / 3
 scene.add(mesh)
 
 /**
